85343ca4983b7c15e8642e641ec65bed1c874164
Koshtak <lorerime101@gmail.com>
2025-11-25 00:31:42 +0300

    Map

--- TowerCrawlerGame/GameCore/Map.cs
+++ TowerCrawlerGame/GameCore/Map.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,41 @@ using System.Threading.Tasks;
 
 namespace GameCore
 {
-    internal class Map
+    public class Map
     {
+        public Tile[,] Grid {  get; private set; }
+        public int Width { get; }
+        public int Height { get; }
+        public Map(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Grid = new Tile[width, height];
+            GenerateEmptyMap();
+
+        }
+        public void GenerateEmptyMap()
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    bool isBorder = (x == 0 || x == Width - 1 || y == 0 || y == Height - 1);
+                    Grid[x, y] = new Tile(x, y, isBorder);
+                }
+            }
+        }
+
+        public bool PlaceEntity(Entity entity, int x, int y)
+        {
+            if(x<0||x>=Width||y<0||y>=Height) return false;
+            Tile targetTile = Grid[x,y];
+            if (targetTile.IsWalkable)
+            {
+                targetTile.Occupant = entity;
+                return true;
+            }
+            return false;
+        }
     }
 }
--- TowerCrawlerGame/GameUI/Program.cs
+++ TowerCrawlerGame/GameUI/Program.cs
@@ -11,19 +11,37 @@ namespace GameUI
     {
         static void Main(string[] args)
         {
+            Map gameMap = new Map(10, 10);
             Player hero = new Player("Marlo", 100, 20,50 );
-            Console.WriteLine($"oyun başladı: {hero.Name}");
-            Console.WriteLine($"Can:{hero.CurrentHP}/{hero.MaxHP}");
-            Console.WriteLine($"Gizlilik:{hero.StealthSkill}");
 
-            Enemy Goblin = new Enemy("japanese goblin", 100, 10, 20);
+            if (gameMap.PlaceEntity(hero, 5, 5)) Console.WriteLine("oyuncu kondu");
+            else Console.WriteLine("konamadı");
+
+            Console.WriteLine($"\n---HARİTA---");
+            for (int y = 0; y < gameMap.Height; y++)
+            {
+                for (int x = 0; x < gameMap.Width; x++)
+                {
+                    Tile t = gameMap.Grid[x, y];
+                    if (t.Occupant == hero) Console.Write("@");
+                    else if (t.IsWall) Console.Write("#");
+                    else Console.Write(".");
+                }
+                Console.WriteLine();
+            }
+
+            /*
+                Enemy Goblin = new Enemy("japanese goblin", 100, 10, 20);
             Console.WriteLine($"\nDüşman Gördün: {Goblin.Name}");
+
+
             bool kactikMi = hero.TryFlee(Goblin);
             if (kactikMi)
                 Console.WriteLine("Kaçtın!");
             else
                 Console.WriteLine("Yakalandın!");
 
+            */
             Console.ReadLine();
         }
     }
